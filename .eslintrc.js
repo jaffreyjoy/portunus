d73c76508fd99a51032fd59b25b01c627f50c1a8
Jaffrey98 <jaffreyjoy@gmail.com>
2019-01-05 11:33:02 +0530

    [chore] add .eslintrc config

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/recommended',
+    '@vue/prettier'
+  ],
+  rules: {
+    'no-console': 'off',
+  }
+}
